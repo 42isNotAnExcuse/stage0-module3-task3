a05d2815324cabf22f00d4f2d6659504e9c97c17
Igor Bugaiov <no-reply>
2022-09-18 03:13:26 -0700

    st0-mod3-t3 1st try

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -2,6 +2,15 @@ package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+
+//        Create 3 int variables with names: first, second, third.
+//        Initialize them with values: 1, 10, 100.
+//        Make console outputs for all of them, each on new line.
+
+        int first = 1;
+        int second = 10;
+        int third = 100;
+
+        System.out.printf("%d\n%d\n%d\n", first, second, third);
     }
 }
--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -2,6 +2,28 @@ package lang.print.gaps.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+//        Declare another 3 variables: first, second, third (1,10,100).
+//        Make console output of 3 initial vars (first, second, third) each on new line.
+//
+//        Initialize vars linkToFirst, linkToSecond, linkToThird with the variables first, second, third.
+//        Reassign (first, second, third) with the : 15, 6, 4.
+//        And write to console all the variables
+//        (in the order they were declared), each on a new line. Pay attention to results.
+
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        int linkToFirst = first;
+        int linkToSecond = second;
+        int linkToThird = third;
+
+        System.out.printf("%d\n%d\n%d\n", first, second, third);
+
+        first = 15;
+        second = 6;
+        third = 4;
+
+        System.out.printf("%d\n%d\n%d\n%d\n%d\n%d\n",
+                first, second, third, linkToFirst, linkToSecond, linkToThird);
     }
 }
